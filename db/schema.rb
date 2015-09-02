259b1c14e6386788ef7fa64f33b7bda3b1fb5c7a
Craigimus1990 <mxwllhllm@gmail.com>
2015-09-02 13:55:32 -0400

    migrated to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150812193748) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "games", force: :cascade do |t|
     t.binary   "board"
     t.integer  "turn"
